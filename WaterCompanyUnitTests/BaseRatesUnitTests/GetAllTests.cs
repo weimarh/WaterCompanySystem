69077abd1b94a5081a77bad55fb2187c9e8a0964
weimarbarea <ggmwba@gmail.com>
2025-10-08 11:34:50 -0700

    Add unit test projects and initial test implementations
    Added `WaterCompanyUnitTests` project to the solution, targeting .NET 8.0, with dependencies on `xUnit`, `Moq`, `MediatR`, and `coverlet.collector`. Updated `WaterCompany.sln` to include the new project and its build configurations.
    
    Modified `appsettings.Development.json` to include `AllowedHosts` and a `ConnectionStrings` section for database configuration.
    
    Implemented `GetAllTests` for `BaseRatesController` to verify behavior when base rates exist and when no base rates exist. Added a placeholder `GetByIdTests` class for future test development.
    
    Created `WaterCompanyTests.csproj` and `WaterCompanyUnitTests.csproj` files with appropriate configurations and project references.

---
+++ WaterCompanyUnitTests/BaseRatesUnitTests/GetAllTests.cs
@@ -0,0 +1,65 @@
+﻿using Application.BaseRates.Common;
+using Application.BaseRates.GetAll;
+using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Web.Api.Controllers;
+
+namespace WaterCompanyUnitTests.BaseRatesUnitTests
+{
+    public class GetAllTests
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly BaseRatesController _controller;
+
+        public GetAllTests()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new BaseRatesController(_mediator.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_WhenBaseRatesExist_ReturnsOkWithBaseRates()
+        {
+            //Arrange
+            var baseRateResponse = new List<BaseRateResponse>
+            {
+                new BaseRateResponse (Guid.NewGuid(), DateTime.Now, "52.36"),
+                new BaseRateResponse (Guid.NewGuid(), DateTime.Now, "10.50"),
+            }.AsReadOnly();
+
+            _mediator.Setup(m => m.Send(It.IsAny<GetAllBaseRatesQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(baseRateResponse);
+
+            //Act
+            var result = await _controller.GetAll();
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedBaseRates = Assert.IsAssignableFrom<IEnumerable<BaseRateResponse>>(okResult.Value);
+            Assert.Equal(2, returnedBaseRates.Count());
+            Assert.Equal("52.36", returnedBaseRates.First().Amount);
+            Assert.Equal("10.50", returnedBaseRates.Last().Amount);
+        }
+
+        [Fact]
+        public async Task GetAll_WhenNoBaseRatesExist_ReturnsOkWithEmptyList()
+        {
+            //Assert
+            var emptyList = new List<BaseRateResponse>();
+
+            _mediator.Setup(m => m.Send(It.IsAny<GetAllBaseRatesQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(emptyList);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedBaseRates = Assert.IsAssignableFrom<IEnumerable<BaseRateResponse>>(okResult.Value);
+            Assert.Empty(returnedBaseRates);
+        }
+
+    }
+}
---
+++ WaterCompanyUnitTests/BaseRatesUnitTests/GetByIdTests.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Moq;
+using Web.Api.Controllers;
+
+namespace WaterCompanyUnitTests.BaseRatesUnitTests
+{
+    public class GetByIdTests
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly BaseRatesController _controller;
+
+        public GetByIdTests(BaseRatesController controller, Mock<IMediator> mediator)
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new BaseRatesController(_mediator.Object);
+        }
+    }
+}
