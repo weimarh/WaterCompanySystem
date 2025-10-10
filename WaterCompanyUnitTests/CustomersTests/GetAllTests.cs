00bcce7ef8565a36a9e02179d645124cf44c0b60
weimarbarea <ggmwba@gmail.com>
2025-10-09 21:18:11 -0700

    Add unit tests for CustomersController endpoints
    Added `GetAllTests` to validate the `GetAll` method behavior, including cases for existing and non-existing customers.
    Added `GetByIdTests` to validate the `GetById` method behavior, covering scenarios for existing customers, not found errors, and invalid IDs.
    Mocked `IMediator` using `Moq` to simulate query handling.
    Used `xUnit` assertions to verify response types, status codes, and returned data.
    Ensured proper verification of `IMediator` calls with expected parameters.

---
+++ WaterCompanyUnitTests/CustomersTests/GetAllTests.cs
@@ -0,0 +1,61 @@
+﻿using Application.Customers.Common;
+using Application.Customers.GetAll;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Web.Api.Controllers;
+
+namespace WaterCompanyUnitTests.CustomersTests
+{
+    public class GetAllTests
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly CustomersController _controller;
+
+        public GetAllTests()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new CustomersController(_mediator.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_WhenCustomersExists_ReturnsOkWithCustomers()
+        {
+            //Arrange
+            var customers = new List<CustomerResponse>
+            {
+                new CustomerResponse("5207907", "Weimar Barea", "70792462"),
+                new CustomerResponse("5296854", "Hermilene Sanchez", "70725926"),
+            }.AsReadOnly();
+
+            _mediator.Setup(m => m.Send(It.IsAny<GetAllCustomersQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(customers);
+
+            //Act
+            var result = await _controller.GetAll();
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCustomers = Assert.IsAssignableFrom<IEnumerable<CustomerResponse>>(okResult.Value);
+            Assert.Equal(2, returnedCustomers.Count());
+        }
+
+        [Fact]
+        public async Task GetAll_WhenNoCustomersExists_ReturnsOkWithEmptyList()
+        {
+            //Arrange
+            var emptyList = new List<CustomerResponse>();
+
+            _mediator.Setup(m => m.Send(It.IsAny<GetAllCustomersQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(emptyList);
+
+            //Act
+            var result = await _controller.GetAll();
+
+            //Arrange
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCustomers = Assert.IsAssignableFrom<IEnumerable<CustomerResponse>>(okResult.Value);
+            Assert.Empty(returnedCustomers);
+        }
+    }
+}
---
+++ WaterCompanyUnitTests/CustomersTests/GetByIdTests.cs
@@ -0,0 +1,92 @@
+﻿using Application.Customers.Common;
+using Application.Customers.GetById;
+using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Web.Api.Controllers;
+
+namespace WaterCompanyUnitTests.CustomersTests
+{
+    public class GetByIdTests
+    {
+        private readonly Mock<IMediator> _mediator;
+        private readonly CustomersController _controller;
+
+        public GetByIdTests()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new CustomersController(_mediator.Object);
+        }
+
+        [Fact]
+        public async Task GetById_WhenCustomerExists_ReturnsOkWithCustomer()
+        {
+            //Arrange
+            var customerId = "5207907";
+            var expectedCustomer = new CustomerResponse(customerId, "Weimar Barea", "70792462");
+
+            _mediator.Setup(m => m.Send(It.Is<GetCustomerByIdQuery>(q => q.CustomerId == customerId), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedCustomer);
+
+            //Act
+            var result = await _controller.GetById(customerId);
+
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedCustomer = Assert.IsType<CustomerResponse>(okResult.Value);
+
+            Assert.Equal(expectedCustomer.CustomerId, returnedCustomer.CustomerId);
+            Assert.Equal(expectedCustomer.CustomerName, returnedCustomer.CustomerName);
+            Assert.Equal(expectedCustomer.PhoneNumber, returnedCustomer.PhoneNumber);
+
+            _mediator.Verify(m => m.Send(It.Is<GetCustomerByIdQuery>(q => q.CustomerId == customerId), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetById_WhenCustomerNotFound_ReturnsProblemWithNotFoundError()
+        {
+            //Arrange
+            var customerId = "5207907";
+            var notFoundError = Error.NotFound("Customer.NotFound", "Customer not found");
+            var errorResult =  ErrorOr<CustomerResponse>.From(new List<Error> {notFoundError});
+
+            _mediator.Setup(m => m.Send(It.Is<GetCustomerByIdQuery>(q => q.CustomerId == customerId), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(errorResult);
+
+            //Act
+            var result = await _controller.GetById(customerId);
+
+            //Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal(500, objectResult.StatusCode);
+
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+            Assert.Contains("Customer not found", problemDetails.Detail);
+
+            _mediator.Verify(m => m.Send(It.Is<GetCustomerByIdQuery>(q => q.CustomerId == customerId), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetById_WithEmptyGuid_ReturnsProblem()
+        {
+            //Arrange
+            var customerId = string.Empty;
+            var validationError = Error.Validation("Customer.BadIdFormat", "Bad format in ID");
+            var errorResult = ErrorOr<CustomerResponse>.From(new List<Error> { validationError });
+
+            _mediator.Setup(m => m.Send(It.Is<GetCustomerByIdQuery>(q => q.CustomerId == customerId), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(errorResult);
+
+            //Act
+            var result = await _controller.GetById(customerId);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            var problemDetails = Assert.IsType<ProblemDetails>(objectResult.Value);
+
+            Assert.Contains("Bad format in ID", problemDetails.Detail);
+
+        }
+    }
+}
